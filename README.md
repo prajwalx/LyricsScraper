37898e66c12b69a402c5e4d5082de972f05a0745
prajwalx <prajwalbhati@hotmail.com>
2019-12-28 01:33:14 +0530

    Bug Fixes: Now using azlyrics.com

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Web Scraping the Lyrics of any Song
 - [Node.js and npm](nodejs.org) Node ^4.2.3, npm ^2.14.7
 
 ### Running
+0. Run `npm install` to install dependencies
 1. Run `node app` to start the development server.
 2. Open Browser and goto `localhost:2000`.
 3. Type the name of song and click Search.
--- routes/routes.js
+++ routes/routes.js
@@ -1,68 +1,65 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var request = require('request');
-var cheerio = require('cheerio');
+const rp = require('request-promise');/* Same as request , with promise support */
+const cheerio = require('cheerio');
 
 router.get('/',function(req, res){
   res.render('index');
 });
 
 router.get('/getmusic/:query',function(req,res){
-//var url1='www.google.co.in/search?='
-console.log(req.params.query);
-var query=""+req.params.query;
-query=query.replace(/ /g,"+");
-var url1="https://www.google.co.in/search?q="+query+"+metrolyrics";
-var metroUrl;
-request(url1,function(error,response,html){
-  // console.log(error);
-    if(!error){
-      console.log(url1);//success
-      var $ = cheerio.load(html);
-      //Now Scrap the first google link
-      //$('.r','a').children[0].href
-      var links = $('a','.r');//Get All Links
-      $(links).each(function(i, link){
-        var text=$(link).text();
-        if(text.search("MetroLyrics")!==-1){
-          metroUrl=$(link).attr('href').substring(7);
-          console.log(metroUrl);
-          return false;//break the loop each
+  console.log(req.params.query);
+  /*Format search query */
+  let query=(req.params.query).toString().trim().replace(/ /g,"+");
+  
+  /*Generate url azlyrics search */
+  let url1=`https://search.azlyrics.com/search.php?q=${query}`
+
+  console.log(url1)
+  
+  /* Visit url1 */
+  rp(url1)
+    .then((html)=>{
+      /*Traverse html DOM */
+      let $ = cheerio.load(html);
+      let panels = $('.panel');/* There are multiple panels like Album , Song,.. */
+      let url2 = '';
+      /* Find Song's panel */
+      panels.each((i,panel)=>{
+        /*Get heading text for this panel */
+        let ph = $(panel).find('.panel-heading b').text();
+        if(ph=='Song results:'){
+          /*Get all anchor tags in this panel */
+          let links = $(panel).find('.text-left>a');//about 20 links
+          url2 = $($(links)[0]).attr('href');//get the first one 
+          return;//break loop
         }
-        //console.log($(link).text() + ':\n  ' + $(link).attr('href'));
-      });//end links scraping
-
-      if(!metroUrl||metroUrl.length<3){
-      res.send('Lyrics Not Found ;(');
-      }
-
-      else{
-        request(metroUrl,function(error,response,html){
-          if(!error){
-            var $ = cheerio.load(html);
-            var data = $('.verse');
-            res.send(data.text());
-            //console.log(data.text());
-          }//end-if !error MetroLyrics
-
-          /*
-          If Error Occurs IN MetroLyrics Scraping
-          */
-          else{
-            res.send('Lyrics Not Found ;(');
-          }
-        });
-      }//end else
-    }//end if(error)
-
-    /*
-    If Error Occurs IN Google Search
-    */
-    else{
-      res.send('Lyrics Not Found ;(');
-    }
-});//end Google Search
+      });
+      /* Send the lyric url to next promise */
+      return url2;
+      
+    })
+    .then((url)=>{
+      console.log(url)
+      /*Visit the Lyrics Page Url */
+      rp(url)
+      .then((html)=>{
+        /*Traverse DOM to scrap lyrics from this page */
+        let $ = cheerio.load(html);
+        let lyrics = $('.ringtone').nextAll().text();
+        /*Send Results */
+        res.send(lyrics);
+      })
+      .catch((err)=>{
+        // console.log(err);
+        res.send('Lyrics Not Found 1;(');
+      });
+    })
+    .catch((err)=>{
+      // console.log(err);
+      res.send('Lyrics Not Found 2;(');
+    })
 
 
 });
